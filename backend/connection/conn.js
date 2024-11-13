41baff77600a154ae6056acddbc5106fb2ef4e0c
1221solomon-tadesse <solomontadesse480@gmail.com>
2024-11-13 10:19:13 +0300

    updated

--- backend/connection/conn.js
+++ backend/connection/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose
   .connect(
-    "mongodb+srv://solomontadesse480:soll@soll.is9ec.mongodb.net/blog?retryWrites=true&w=majority&appName=soll"
+    "mongodb+srv://solomontadesse480:soll@soll.is9ec.mongodb.net/blog?retryWrites=true&w=majority&appName=soll",
   )
   .then(() => console.log("connected"));
